c2dce30b40cb0e0f15f4f5490c52a0345991c306
liufei <649480725@qq.com>
2018-05-18 18:21:51 +0800

    学习
    学习

---
+++ src/main/java/com/liu/demo/config/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.liu.demo.config;
+
+import com.liu.demo.bean.ErrorInfo;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        ErrorInfo<String> r = new ErrorInfo<>();
+        r.setMessage(e.getMessage());
+        r.setCode(ErrorInfo.ERROR);
+        r.setData("Some Data");
+        r.setUrl(req.getRequestURL().toString());
+        return r;
+    }
+}
---
+++ src/main/java/com/liu/demo/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.liu.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public  String index(){
+        return  "Hello World";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        int a =  1 / 0 ;
+        return "index";
+    }
+}

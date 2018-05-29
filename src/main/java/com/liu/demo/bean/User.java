8ec300265022e7732ca70bb6cd51043947e78590
liufei <649480725@qq.com>
2018-05-29 16:05:02 +0800

    spring boot demo
    spring boot demo

--- src/main/java/com/liu/demo/bean/User.java
+++ src/main/java/com/liu/demo/bean/User.java
@@ -48,5 +48,16 @@ public class User {
         this.age = age;
     }
 
+    public enum States {
+        UNPAID,                 // 待支付
+        WAITING_FOR_RECEIVE,    // 待收货
+        DONE                    // 结束
+    }
+
+    public enum Events {
+        PAY,        // 支付
+        RECEIVE     // 收货
+    }
+
 
 }
